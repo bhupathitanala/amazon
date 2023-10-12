0ecd59cb15eb7c14255f1a4d64ad272e3c842c7d
bhupathi <bhupathicse@gmail.com>
2023-10-12 10:26:36 +0530

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>vasavi save to reload.
+          Vsavi Engineering college
         </p>
         <a
           className="App-link"
